240d1dfd2cc7d3f1ab35f4d5971ec233c15796b1
D22126473 Aleksi Hannula <D22126473@mydit.ie>
2022-10-21 12:49:21 +0100

    Packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
 import './App.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { SpeedDial } from '@mui/material'
 
 function App() {
   return (
     <div className="App">
       <MapContainer center={[53.3498, -6.2603]} zoom={11}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <SpeedDial></SpeedDial>
       </MapContainer>
     </div>
   )
